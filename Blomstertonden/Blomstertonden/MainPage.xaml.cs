cf19e15840c24824cb465bfa0d591cd7ca7a4b8d
Greg Bisbjerg <gbisbjerg@gmail.com>
2018-04-24 10:41:39 +0200

    LoginPage

--- Blomstertonden/Blomstertonden/MainPage.xaml.cs
+++ Blomstertonden/Blomstertonden/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.ApplicationModel.Core;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Blomstertonden
@@ -25,6 +26,36 @@ namespace Blomstertonden
         public MainPage()
         {
             this.InitializeComponent();
+
+            //ApplicationView.GetForCurrentView().Title = "TEST";
+            CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+            // Set the BackgroundElement instead of the entire Titlebar grid
+            // so that we can add clickable element in title bar.
+            Window.Current.SetTitleBar(BackgroundElement);
+
+            String path = Directory.GetCurrentDirectory() + @"\FlipView";
+            LoginFV.ItemsSource = Directory.GetFiles(path).Select(p => "ms-appx:///" + p);
+            int change = 1;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(7);
+            timer.Tick += (o, a) =>
+            {
+                // If we'd go out of bounds then reverse
+                int newIndex = LoginFV.SelectedIndex + change;
+                if (newIndex >= LoginFV.Items.Count || newIndex < 0)
+                {
+                    change *= -1;
+                }
+
+                LoginFV.SelectedIndex += change;
+            };
+
+            timer.Start();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(CreateOrder), null);
         }
     }
 }
