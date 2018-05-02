4ca2ac6ad7b2622aec6264a646b2433e6b8c8c17
cybon <kimon5@hotmail.dk>
2018-05-02 15:15:12 +0200

    adasd

---
+++ Blomstertonden/Blomstertonden/AppBase/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blomstertonden.AppBase
+{
+    public static class AppConfig
+    {
+        private static string _serverURL = "";
+
+        public static string ServerURL
+        {
+            get { return _serverURL; }
+        }
+    }
+}
