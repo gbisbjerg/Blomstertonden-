1f9665e6edbbf66a4d2c5e45af366cde52fcc876
cybon <kimon5@hotmail.dk>
2018-05-02 11:54:17 +0200

    gernics implemented

---
+++ Blomstertonden/Blomstertonden/Generics/CommandsBase/CreateCommandBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsLibrary
+{
+    public class CreateCommandBase<TData, T, TKey> : CommandBase<TData, T, TKey>
+        where TData : IKey<TKey>, new()
+        where T : IKey<TKey>, new()
+    {
+        public CreateCommandBase(ICRUD<T, TData, TKey> catalog, MasterDetailsViewModelBase<TData, T, TKey> viewModel)
+        :base(catalog, viewModel){}
+        public override void ExecuteEvent()
+        {
+            _viewModel.RefreshItemViewModelCollection();
+        }
+        public override bool CanExecute()
+        {
+            return true;
+        }
+        public override void Execute()
+        {
+            _catalog.Create(_viewModel.DataPackage);
+            ExecuteEvent();
+        }
+    }
+}
---
+++ Blomstertonden/Blomstertonden/Generics/Interfaces/IDBSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsLibrary
+{
+    public interface IDBSource<T, TKey>
+    {
+        Task Create(T obj);
+        Task<T> Read(TKey key);
+        Task Update(T obj);
+        Task Delete(TKey key);
+        Task<Dictionary<TKey, T>> Load();
+    }
+}
