e88356bda9d9b7d6ab0904835f20ab5c2aac682e
cybon <kimon5@hotmail.dk>
2018-05-11 11:07:54 +0200

    uhu

--- Blomstertonden/Blomstertonden/DomainModels/ModelFactories/OrderFactory.cs
+++ Blomstertonden/Blomstertonden/DomainModels/ModelFactories/OrderFactory.cs
@@ -11,7 +11,18 @@ namespace Blomstertonden
     {
         public Order Convert(OrderTData data)
         {
-            throw new NotImplementedException();
+            Order obj = new Order();
+            obj.Description = data.Description;
+            obj.Date = data.Date;
+            obj.DeliveryDate = data.DeliveryDate;
+            obj.Street = data.Street;
+            obj.TotalPrice = data.TotalPrice;
+            obj.CardMessage = data.CardMessage;
+            obj.FK_Customer = data.FK_Customer;
+            obj.FK_PaymentType = data.FK_PaymentType;
+            obj.FK_City = data.FK_City;
+            obj.FK_Status = data.FK_Status;
+            return obj;
         }
     }
 }
