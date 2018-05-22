7c10b8a3f879109b026c45383d2a9d6a30ce7b89
Greg Bisbjerg <gbisbjerg@gmail.com>
2018-05-22 13:25:59 +0200

    jklljk

---
+++ Blomstertonden/Blomstertonden/Controllers/TData/OrderedProductTData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericsLibrary;
+
+namespace Blomstertonden
+{
+    public class OrderedProductTData : IKey<Tuple<int, int>>
+    {
+        public Tuple<int, int> Key { get =>  new Tuple<int, int>(FK_Order,FK_Product); set => Key = value; }
+
+        public int FK_Order { get; set; }
+
+        public int FK_Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ Blomstertonden/Blomstertonden/DomainModels/Catalogs/OrderedProductCatalog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericsLibrary;
+
+namespace Blomstertonden
+{
+    public class OrderedProductCatalog : AppCatalogBase<OrderedProduct, OrderedProductTData, Tuple<int, int> >
+    {
+        public List<OrderedProductTData> _orderedProductTDataList;
+
+        private static OrderedProductCatalog _instance;
+
+        public OrderedProductCatalog( string apiId) : base(new OrderedProductFactory(), apiId)
+        {
+            _orderedProductTDataList = new List<OrderedProductTData>();
+        }
+
+        public static OrderedProductCatalog Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new OrderedProductCatalog("OrderedProducts");
+                }
+                return _instance;
+            }
+        }
+
+        public List<OrderedProductTData> OPTDataList { get => _orderedProductTDataList; set => _orderedProductTDataList = value; }
+    }
+}
